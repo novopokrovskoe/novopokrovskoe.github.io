b8828680af4c607f293b79239fc568b6ebea8445
Константин Кухарев <Konstantin.kukharev@gmail.com>
2023-10-11 19:49:26 +0300

    get link

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,40 @@
-import React from 'react'
+//import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import Login from './Login'
-import Error from './Error'
-import './css/index.scss'
-import backendSettings from "./module/settings/backend"
-import settings from "./module/settings/app"
+// import App from './App'
+// import Login from './Login'
+// import Error from './Error'
+// import './css/index.scss'
+// import backendSettings from "./module/settings/backend"
+// import settings from "./module/settings/app"
 
-import { Provider } from 'react-redux'
-import { Route, Switch } from 'react-router'
-import { ConnectedRouter } from 'connected-react-router'
-import RoutePrivate from './module/RoutePrivate'
-import configureStore, { history } from './redux/storeConfig'
+// import { Provider } from 'react-redux'
+// import { Route, Switch } from 'react-router'
+// import { ConnectedRouter } from 'connected-react-router'
+// import RoutePrivate from './module/RoutePrivate'
+// import configureStore, { history } from './redux/storeConfig'
 
-import AuthVerification from "./module/auth/ya/verification";
+// import AuthVerification from "./module/auth/ya/verification";
 
-const store = configureStore({})
+// const store = configureStore({})
 
-const appSettings :settings = {
-    basePath: process.env.REACT_APP_BASE_PATH ? `${process.env.REACT_APP_BASE_PATH}` : '',
-}
+// const appSettings :settings = {
+//     basePath: process.env.REACT_APP_BASE_PATH ? `${process.env.REACT_APP_BASE_PATH}` : '',
+// }
 
-const apiSettings :backendSettings = {
-    tokenKey: process.env.REACT_APP_TOKEN_KEY ? `${process.env.REACT_APP_TOKEN_KEY}` : '1',
-    cookieKey: process.env.REACT_APP_COOKIE_KEY ? `${process.env.REACT_APP_COOKIE_KEY}` : '',
-    publicKey: process.env.REACT_APP_PUBLIC_KEY ? `${process.env.REACT_APP_PUBLIC_KEY}` : '',
-    api: {
-        basePath: process.env.REACT_APP_API_PATH ? `${process.env.REACT_APP_API_PATH}` : '',
-        domain: process.env.REACT_APP_CURRENT_DOMAIN ? `${process.env.REACT_APP_CURRENT_DOMAIN}` : ''
-    },
-    ya: {
-        clientID: process.env.REACT_APP_YA_CID ? `${process.env.REACT_APP_YA_CID}` : '',
-        clientSecret: process.env.REACT_APP_YA_CIS ? `${process.env.REACT_APP_YA_CIS}` : '',
-        callbackURL: process.env.REACT_APP_YA_CURI ? `${process.env.REACT_APP_YA_CURI}` : '',
-    }
-}
+// const apiSettings :backendSettings = {
+//     tokenKey: process.env.REACT_APP_TOKEN_KEY ? `${process.env.REACT_APP_TOKEN_KEY}` : '1',
+//     cookieKey: process.env.REACT_APP_COOKIE_KEY ? `${process.env.REACT_APP_COOKIE_KEY}` : '',
+//     publicKey: process.env.REACT_APP_PUBLIC_KEY ? `${process.env.REACT_APP_PUBLIC_KEY}` : '',
+//     api: {
+//         basePath: process.env.REACT_APP_API_PATH ? `${process.env.REACT_APP_API_PATH}` : '',
+//         domain: process.env.REACT_APP_CURRENT_DOMAIN ? `${process.env.REACT_APP_CURRENT_DOMAIN}` : ''
+//     },
+//     ya: {
+//         clientID: process.env.REACT_APP_YA_CID ? `${process.env.REACT_APP_YA_CID}` : '',
+//         clientSecret: process.env.REACT_APP_YA_CIS ? `${process.env.REACT_APP_YA_CIS}` : '',
+//         callbackURL: process.env.REACT_APP_YA_CURI ? `${process.env.REACT_APP_YA_CURI}` : '',
+//     }
+// }
 
 ReactDOM.render(
     // <Provider store={store}>
