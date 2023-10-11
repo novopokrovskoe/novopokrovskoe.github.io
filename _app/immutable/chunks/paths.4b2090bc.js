3b1887c4a5ce449fcfe025ebbcb28afdc5b1dee4
Константин Кухарев <Konstantin.kukharev@gmail.com>
2023-10-11 23:35:43 +0300

    Updates

--- _app/immutable/chunks/paths.4b2090bc.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1p3l7p9)==null?void 0:s.base)??"/novopokrovskoe.github.io";var o;const t=((o=globalThis.__sveltekit_1p3l7p9)==null?void 0:o.assets)??e;export{t as a,e as b};
